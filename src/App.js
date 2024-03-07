9578cee91310f5fa33345d6491a9bb68bfe420ce
Ahmed <ahmed@Musds-MacBook-Pro.local>
2024-03-07 20:40:52 +0000

    this is a react folder to practicing my react skills after having completed a react course previously.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <User
+        firstName="Ahmed"
+        surName="Abdulrahman"
+        email="ahmedabdi03@hotmail.com"
+        age={20}
+      />
+      <User
+        firstName="Hanad"
+        surName="Abdulrahman"
+        email="hanadabdi236@hotmail.com"
+        age={24}
+      />
+      <User />
     </div>
   );
 }
+
+const User = (props) => {
+  return (
+    <div>
+      <h1>{props.firstName}</h1>
+      <h1>{props.surName}</h1>
+      <h2>{props.email}</h2>
+      <h3>{props.age}</h3>
+    </div>
+  );
+};
 
 export default App;
