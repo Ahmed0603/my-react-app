74cba8a3d6cb95bec2d3380471bdedda8f7689ad
Ahmed Abdulrahman <121971562+Ahmed0603@users.noreply.github.com>
2024-03-21 15:23:17 +0000

    Add files via upload
    This marks the beginning of my first react project which is a To-Do List. This will comprise of the skills I have been learning and uploading for the past 2 week, such as states, conditional and/or ternary operators. This current upload displays the basis of the To-Do List; rendering lists to the UI.

--- App.js
+++ App.js
@@ -1,27 +1,30 @@
 import "./App.css";
 import { useState } from "react";
-
 function App() {
-  const [count, setCount] = useState(0);
-  const increase = () => {
-    setCount(count + 1);
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
   };
 
-  const decrease = () => {
-    setCount(count - 1);
-  };
-
-  const setToZero = () => {
-    setCount(!count && 0);
+  const addTask = () => {
+    const newTodoList = [...todoList, newTask];
+    setTodoList(newTodoList);
   };
 
   return (
     <div className="App">
-      <button onClick={increase}> Increase </button>
-      <button onClick={decrease}> Decrease </button>
-      <button onClick={setToZero}> Set to Zero </button>
-      {count}
+      <div className="addTasks">
+        <input type="text" onChange={handleChange} />
+        <button onClick={addTask}> Add Task </button>
+      </div>
+      <div className="list"></div>
+      {todoList.map((task) => {
+        return <h1>{task}</h1>;
+      })}
     </div>
   );
 }
+
 export default App;
