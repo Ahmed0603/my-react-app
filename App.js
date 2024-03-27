b70a86fe21aa94f49aa92f5e5d65a5f145b646a1
Ahmed Abdulrahman <121971562+Ahmed0603@users.noreply.github.com>
2024-03-27 23:38:04 +0000

    Add files via upload

--- App.js
+++ App.js
@@ -1,28 +1,19 @@
 import "./App.css";
 import { useState } from "react";
+import { Text } from "./Text";
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [newTask, setNewTask] = useState("");
-
-  const handleChange = (event) => {
-    setNewTask(event.target.value);
-  };
-
-  const addTask = () => {
-    const newTodoList = [...todoList, newTask];
-    setTodoList(newTodoList);
-  };
+  const [showText, setShowText] = useState(false);
 
   return (
     <div className="App">
-      <div className="addTasks">
-        <input type="text" onChange={handleChange} />
-        <button onClick={addTask}> Add Task </button>
-      </div>
-      <div className="list"></div>
-      {todoList.map((task) => {
-        return <h1>{task}</h1>;
-      })}
+      <button
+        onClick={() => {
+          setShowText(!showText);
+        }}>
+        {" "}
+        Show Text
+      </button>
+      {showText && <Text />}
     </div>
   );
 }
