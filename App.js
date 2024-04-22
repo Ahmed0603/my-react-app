897c350f7d782eb14791f178fdb782be5ff9e6fb
Ahmed Abdulrahman <121971562+Ahmed0603@users.noreply.github.com>
2024-04-22 16:00:06 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Home } from "./pages/Home";
-import { Menu } from "./pages/Menu";
 import { Contact } from "./pages/Contact";
 import { Error } from "./pages/Error";
 import { Navbar } from "./pages/Navbar";
+import { Profile } from "./pages/Profile";
+import { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("AhmedTech!");
   return (
     <div className="App">
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
+          <Route path="/" element={<Home username={username} />} />
+          <Route
+            path="/profile"
+            element={<Profile username={username} setUsername={setUsername} />}
+          />
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<Error />} />
         </Routes>
@@ -21,4 +26,30 @@ function App() {
     </div>
   );
 }
+
 export default App;
+
+// import { useState } from "react";
+// import { AppContext } from "../App";
+// import { useContext } from "react";
+
+// export const ChangeProfile = (props) => {
+//   const { setUsername } = useContext(AppContext);
+
+//   const [newUsername, setNewUsername] = useState("");
+//   return (
+//     <div>
+//       <input
+//         onChange={(event) => {
+//           setNewUsername(event.target.value);
+//         }}
+//       />
+//       <button
+//         onClick={() => {
+//           props.setUsername(newUsername);
+//         }}>
+//         Change Username
+//       </button>
+//     </div>
+//   );
+// };
