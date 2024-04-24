aa820ef62da76c7efd5c1cbee16fca9ca8ef178b
Ahmed Abdulrahman <121971562+Ahmed0603@users.noreply.github.com>
2024-04-24 21:10:36 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -6,23 +6,33 @@ import { Error } from "./pages/Error";
 import { Navbar } from "./pages/Navbar";
 import { Profile } from "./pages/Profile";
 import { useState, createContext } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const AppContext = createContext();
 function App() {
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
   const [username, setUsername] = useState("AhmedTech!");
   return (
     <div className="App">
-      <AppContext.Provider value={{ username, setUsername }}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<Error />} />
-          </Routes>
-        </Router>
-      </AppContext.Provider>
+      <QueryClientProvider client={client}>
+        <AppContext.Provider value={{ username, setUsername }}>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="*" element={<Error />} />
+            </Routes>
+          </Router>
+        </AppContext.Provider>
+      </QueryClientProvider>
     </div>
   );
 }
